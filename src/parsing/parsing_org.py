7d2e79208ced9b987b3973c1323bf0f2391b38bb
Gleb Budnik <budnikgleb@mail.ru>
2023-07-04 22:56:00 +1000

    add test parsing org

---
+++ src/parsing/parsing_org.py
@@ -0,0 +1,16 @@
+import os
+import logging
+import re
+import time
+
+from tqdm import tqdm
+import requests
+import pandas as pd
+import bs4.element
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+class ParsingOrg:
+    def __init__(self, num_contract_file):
+        pass
